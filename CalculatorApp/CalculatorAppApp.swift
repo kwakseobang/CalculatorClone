4b52d3fd21bb6084e392a417e8027f58e0b9f2b0
Kwakmunsu <iii6602@naver.com>
2024-03-04 21:58:24 +0900

    test

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by 곽서방 on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

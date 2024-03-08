a8c70143e5faae58d21398921cfec0c8fccfcfd6
Kwakmunsu <iii6602@naver.com>
2024-03-08 21:57:43 +0900

    계산기 UI 리팩토링 완료 반복문 사용

--- CalculatorApp/ContentView.swift
+++ CalculatorApp/ContentView.swift
@@ -8,188 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var totalNumber: String = "0"
+    private var btnData: [[String]] = [
+        ["C","%","//","$"],
+        ["7","8","9","X"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0","0",".","="],
+     ]
     var body: some View {
         ZStack{
-            Color.black.ignoresSafeArea()
+            Color.black.ignoresSafeArea()  // 배경 검은색으로
             VStack {
                 Spacer()
                 HStack {
                     Spacer()
-                    Text("123,456")
+                    Text(totalNumber)
                         .padding()
                         .font(.system(size: 70))
                         .foregroundColor(.white)
                 }
-                HStack {
-                    .modifier(NumberBtnViewModifier(color:.gray, name:"C", action: {
-                        Text("")
-                    }) as! View)
-                    Text("^^")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("%")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("//")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.orange.opacity(1)) // opacity 투명도
-                        )
-                }
-                HStack {
-                    Text("7")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("8")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("9")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("X")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.orange.opacity(1)) // opacity 투명도
-                        )
-                }
-                
-                HStack {
-                    Text("4")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("5")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("6")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("-")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.orange.opacity(1)) // opacity 투명도
-                        )
-                }
-                
-                HStack {
-                    Text("1")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("2")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("3")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("+")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.orange.opacity(1)) // opacity 투명도
-                        )
-                }
-                
-                HStack {
-                    Text("0")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("0")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text(".")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.gray.opacity(1)) // opacity 투명도
-                        )
-                    Text("=")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .frame(width: 80,height: 80)
-                        .background(
-                            Circle()
-                                .fill(Color.orange.opacity(1)) // opacity 투명도
-                        )
+                ForEach(btnData, id: \.self) { line in
+                    HStack {
+                        ForEach(line, id: \.self) { item in
+                            Button{
+                                if totalNumber == "0"{
+                                    totalNumber = "7"
+                                }else {
+                                    totalNumber += "7"
+                                }
+                            }label: {
+                                Text("\(item)")
+                                    .font(.system(size: 30))
+                                    .foregroundColor(.white)
+                                    .frame(width: 80,height: 80)
+                                    .background(
+                                        Circle()
+                                            .fill(Color.customNumBtn.opacity(1)) // opacity 투명도
+                                    )
+                            }
+                        }
+                    }
                 }
             }
-            .padding()
         }
     }
 }
---
+++ CalculatorApp/Extension/Color+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extension.swift
+//  CalculatorApp
+//
+//  Created by 곽서방 on 3/8/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let customNumBtn: Color = .init("NumberBtn")
+}

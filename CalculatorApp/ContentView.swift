10925e6f28e9fca5574670cc4528f0f325f0d725
Kwakmunsu <iii6602@naver.com>
2024-03-09 13:32:06 +0900

    enum 활용해서 코드 간결화

--- CalculatorApp/ContentView.swift
+++ CalculatorApp/ContentView.swift
@@ -7,15 +7,83 @@
 
 import SwiftUI
 
+enum BtnType: String {
+    case first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, zero
+    case dot, equal, plus, minus, multiple, devide
+    case percent, opposite, clear
+    
+    var btnDisplayName: String {
+        switch self {
+        case .first :
+            return "1"
+        case .second :
+            return "2"
+        case .third :
+            return "3"
+        case .fourth :
+            return "4"
+        case .fifth :
+            return "5"
+        case .sixth :
+            return "6"
+        case .seventh :
+            return "7"
+        case .eighth :
+            return "8"
+        case .nineth :
+            return "9"
+        case .zero :
+            return "0"
+        case .dot :
+            return "."
+        case .equal :
+            return "="
+        case .plus :
+            return "+"
+        case .minus :
+            return "-"
+        case .multiple :
+            return "X"
+        case .devide :
+            return "$"
+        case .percent :
+            return "%"
+        case .opposite :
+            return "/"
+        case .clear :
+            return "C"
+        }
+    }
+    var backgroundColor: Color {
+        switch self {
+        case .first,.second,.third,.fourth,.fifth,.sixth,.seventh,.eighth,.nineth,.zero,.dot:
+            return Color(.numberBtn)
+        case .equal, .plus, .minus, .multiple, .devide:
+            return Color(.orange)
+        case .percent, .opposite, .clear:
+            return Color(.gray)
+        }
+    }
+    var foregroundColor: Color  {
+        switch self {
+        case .first,.second,.third,.fourth,.fifth,.sixth,.seventh,.eighth,.nineth,.zero,.dot,.equal, .plus, .minus, .multiple, .devide:
+            return Color(.white)
+        case .percent, .opposite, .clear:
+            return Color(.black)
+        }
+    }
+}
 struct ContentView: View {
     @State private var totalNumber: String = "0"
-    private var btnData: [[String]] = [
-        ["C","%","//","$"],
-        ["7","8","9","X"],
-        ["4","5","6","-"],
-        ["1","2","3","+"],
-        ["0","0",".","="],
-     ]
+    private var btnData: [[BtnType]] = [
+        [.clear,.opposite,.percent,.devide],
+        [.seventh,.eighth,.nineth,.multiple],
+        [.fourth,.fifth,.sixth,.minus],
+        [.first,.second,.third,.plus],
+        [.zero,.zero,.dot,.equal],
+    ]
+    
+    
     var body: some View {
         ZStack{
             Color.black.ignoresSafeArea()  // 배경 검은색으로
@@ -38,13 +106,13 @@ struct ContentView: View {
                                     totalNumber += "7"
                                 }
                             }label: {
-                                Text("\(item)")
+                                Text("\(item.btnDisplayName)")
                                     .font(.system(size: 30))
                                     .foregroundColor(.white)
                                     .frame(width: 80,height: 80)
                                     .background(
                                         Circle()
-                                            .fill(Color.customNumBtn.opacity(1)) // opacity 투명도
+                                            .fill(item.backgroundColor.opacity(1)) // opacity 투명도
                                     )
                             }
                         }
